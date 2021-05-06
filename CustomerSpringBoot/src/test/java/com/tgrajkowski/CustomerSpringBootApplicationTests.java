f66e4324ec65aa12b9e64cfa36a24a33944bf45c
tomek401273 <tomek371240@gmail.com>
2021-05-06 10:25:14 +0200

    CustomerSpringBoot init

---
+++ CustomerSpringBoot/src/test/java/com/tgrajkowski/CustomerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgrajkowski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
